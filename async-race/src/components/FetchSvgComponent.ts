43ef7d937c2acbca8525669519d534bbe5e76fed
ketrololz <bqnexxx@gmail.com>
2025-04-09 18:06:45 +0300

    fix: add svg cache

--- async-race/src/components/FetchSvgComponent.ts
+++ async-race/src/components/FetchSvgComponent.ts
@@ -1,8 +1,10 @@
-import { SVG_PATH } from "../constants/app-settings";
-import type { Props } from "../types/props";
-import BaseComponent from "./base-component";
+import { SVG_PATH } from '../constants/app-settings';
+import type { Props } from '../types/props';
+import BaseComponent from './base-component';
+type cache = { [key: string]: string };
 
 export class FetchSvgComponent extends BaseComponent<'div'> {
+  private cache: cache = {};
   public svgText = '';
 
   constructor(name: string, props: Props<'div'> = {}) {
@@ -11,7 +13,11 @@ export class FetchSvgComponent extends BaseComponent<'div'> {
   }
 
   private async render(name: string): Promise<void> {
-    try{
+    if (this.cache[name]) {
+      this.node.innerHTML = this.cache[name];
+      return;
+    }
+    try {
       const svg = await fetch(`${SVG_PATH}/${name}.svg`);
       this.svgText = await svg.text();
       this.node.innerHTML = this.svgText;
